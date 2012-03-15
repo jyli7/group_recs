e620021ccfd1b9326bc138120f68fa0f51714d27
Jordi Noguera <jordinoguera83@gmail.com>
2012-03-15 09:17:34 -0700

    forgot to add recommendation

--- app/models/rec.rb
+++ app/models/rec.rb
@@ -1,15 +1,13 @@
 class Rec
-  attr_accessor :name, :title, :description, :image_url, :url, :rating, :trailer, :rt_rating, :imdb_rating
+  attr_accessor :title, :image_url, :rating, :trailer, :synopsis, :directed_by
   
   def initialize(rec)
     @name = rec[:name]
     @title = rec[:title]
-    @description = rec[:description]
     @image_url = rec[:image_url]
-    @url = rec[:url]
     @rating = rec[:rating]
     @trailer = rec[:trailer] || "No trailer available"
-    @rt_rating = rec[:rt_rating]
-    @imdb_rating = rec[:imdb_rating]
+    @synopsis = rec[:synopsis]
+    @directed_by = rec[:directed_by]
   end
 end
