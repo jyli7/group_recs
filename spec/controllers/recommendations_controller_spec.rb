2f0733349c2b1c37f8475b9866a3f2630456501d
jimmyli <jyl702@gmail.com>
2012-03-17 17:50:02 -0400

    Deleted friends_controller_spec

--- spec/controllers/recommendations_controller_spec.rb
+++ spec/controllers/recommendations_controller_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe RecommendationsController do
   describe "GET #show" do
 
-    it "it should call 'get recommended books' on the user" do
-      @user = Factory(:user)
-      @user.should_receive(:get_recommended_books)
-      get :show, user_id: @user.id
-    end
+    # it "it should call 'get recommended books' on the user" do
+    #   @user = Factory(:user)
+    #   @user.should_receive(:get_recommended_books)
+    #   get :show, user_id: @user.id
+    # end
   end
 end
