d8c3324e18251f8e737d4a6b17fb5b65fa71a3e0
Jordi Noguera <jordinoguera83@gmail.com>
2012-03-20 18:30:57 -0700

    Limit friends to 2K and recommendations to 25

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ActiveRecord::Base
     self.hunch_id = value
   end
   
-  def friends(limit = 20)
+  def friends(limit = 2000)
     @friends ||= HunchAPI.get_friends(limit).map { |friend| Friend.new(friend) }.sort
   end
   
-  def group_recs(user_ids, limit = 15)
+  def group_recs(user_ids, limit = 25)
     @group_recs ||= HunchAPI.get_group_recs(user_ids, limit).map { |rec| Rec.new(rec) }
   end
 
