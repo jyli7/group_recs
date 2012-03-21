4cb52e572e747395a6a889386e148079c7fb5667
Jordi Noguera <jordinoguera83@gmail.com>
2012-03-20 23:22:41 -0700

    is it domains instead

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Groupit::Application.config.session_store :cookie_store, key: '_groupit_session', domain: :all
+Groupit::Application.config.session_store :cookie_store, key: '_groupit_session', domains: :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
