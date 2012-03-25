0a48588be627fad59a596455494d425c31c6641b
Jordi Noguera <jordinoguera83@gmail.com>
2012-03-24 17:40:45 -0700

    still trying to expire cache if user has no friends

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,21 @@
 class HomeController < ApplicationController
 
   caches_action :index, :layout => :false, :cache_path => Proc.new { |c| user_path(c.send(:current_user).id) }, :expires_in => 10.minutes
+  after_filter :expire_cache, :unless => :has_friends?
 
   def index
     @friends = current_user.friends
-    unless has_friends?
-      expire_fragment(user_path(current_user.id))
-      render :no_friends
-    end
+    render :no_friends unless has_friends?
   end
 
   private
 
   def has_friends?
     current_user.friends.any?
+  end
+
+  def expire_cache
+    expire_fragment(user_path(current_user.id))
   end
 
 end
