6617c00cfa9afe69a6ea7eb55c7bc18679724aa0
Jordi Noguera <jordinoguera83@gmail.com>
2012-03-24 17:32:23 -0700

    whoopsie with the cache

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
 
-  caches_action :index, :layout => :false, :cache_path => Proc.new { |c| user_url(c.send(:current_user).id) }, :expires_in => 10.minutes
+  caches_action :index, :layout => :false, :cache_path => Proc.new { |c| user_path(c.send(:current_user).id) }, :expires_in => 10.minutes
 
   def index
     @friends = current_user.friends
     unless has_friends?
-      expire_fragment(user_url(current_user).id)
+      expire_fragment(user_path(current_user.id))
       render :no_friends
     end
   end
