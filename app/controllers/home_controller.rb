2beb06cfc223b87a62a5ff51c322f2114a1b9e7c
Jordi Noguera <jordinoguera83@gmail.com>
2012-03-24 18:26:06 -0700

    layout => false in the cache

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 
-  caches_action :index, :if => [:current_user, :has_friends?], :layout => :false, :cache_path => Proc.new { |c| user_path(c.send(:current_user).id) }, :expires_in => 10.minutes
+  caches_action :index, :if => [:current_user, :has_friends?], :layout => false, :cache_path => Proc.new { |c| user_path(c.send(:current_user).id) }, :expires_in => 10.minutes
 
   def index
     @friends = current_user.friends
