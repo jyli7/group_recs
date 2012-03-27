3efb0acdfccf93c15147c475768fe63423d8fa24
Jordi Noguera <jordinoguera83@gmail.com>
2012-03-26 23:30:05 -0700

    removing caching

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
 
-  caches_action :index, :if => [:current_user, :has_friends?], :layout => false, :cache_path => Proc.new { |c| user_path(c.send(:current_user).id) }, :expires_in => 10.minutes
-
   def index
     @friends = current_user.friends
     current_user.update_attribute(:has_friends, true) if !current_user.has_friends && @friends.any?
